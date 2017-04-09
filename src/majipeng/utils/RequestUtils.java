14bb8a54b5ba4499b5aa8ca2d17455106c2dcd25
majipeng <1402152841@qq.com>
2017-04-09 17:20:42 +0800

    change to spdy

--- src/majipeng/utils/RequestUtils.java
+++ src/majipeng/utils/RequestUtils.java
@@ -2,47 +2,43 @@ package majipeng.utils;
 
 import java.net.URISyntaxException;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import majipeng.protocol.Header;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
 import majipeng.protocol.Protocol;
-import majipeng.protocol.Protocol.MimeType;
-import majipeng.protocol.RequestLine;
-import majipeng.protocol.RequestLine.Mothed;
-import majipeng.protocol.UriUtils;
 
 public class RequestUtils {
 
-	public static void send(ChannelHandlerContext context, String mothed, java.net.URI url, String content,
-			Header... headers) {
-		final Protocol request = Protocol.obtain();
-		request.setRequestLine(new RequestLine(mothed, url.toString()));
-		if (headers != null) {
-			for (Header s : headers) {
-				request.addHeader(s);
-			}
-		}
-		request.setContent(content);
+	public static void send(ChannelHandlerContext context, String url,String content,String contentType) {
+        HttpRequest request= createBaseRequest(content,contentType);
 		send(context, request);
 	}
 
 	public static void sendPing(ChannelHandlerContext context) throws URISyntaxException {
-		send(context, Mothed.OPTIONS, UriUtils.create(Protocol.PATH_PING), null,
-				new Header(Protocol.HEADER_TYPE, MimeType.NULL));
+		send(context,Protocol.PATH_PING,null,"text/plain");
 	}
 
-	public static void send(ChannelHandlerContext context, final Protocol request) {
+	public static void send(ChannelHandlerContext context,  HttpRequest request) {
 		send(context.channel(), request);
 	}
 
-	public static void send(Channel ch, final Protocol request) {
-		ch.writeAndFlush(request.toJson()).addListener(new ChannelFutureListener() {
-			@Override
-			public void operationComplete(ChannelFuture arg0) throws Exception {
-				request.recycle();
-			}
-		});
+	public static void send(Channel ch,  HttpRequest request) {
+		ch.writeAndFlush(request);
 	}
+
+    private static HttpRequest createBaseRequest(String str,String contentType){
+        ByteBuf content= Unpooled.copiedBuffer(str, CharsetUtil.UTF_8);
+        HttpRequest request=new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET,Protocol.PATH_PING,content);
+        request.headers().set(HttpHeaderNames.CONTENT_TYPE,contentType+";charset=UTF-8");
+        request.headers().set(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
+        return request;
+    }
 }
